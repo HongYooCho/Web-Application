0e6852f3ae70b0db5a0baaf64e1395945bc4a5ed
Seo-Hyung <westbro00@naver.com>
2017-09-19 20:53:03 +0900

    commit

---
+++ project/script.js
@@ -0,0 +1,9 @@
+
+wbtn=document.getElementById('whilte_btn');
+wbtn.addEventListener('click', function(){
+  document.getElementById('target').className='white';
+})
+bbtn=document.getElementById('black_btn');
+bbtn.addEventListener('click', function(){
+  document.getElementById('target').className='black';
+})
